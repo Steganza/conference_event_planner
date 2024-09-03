2b8267664f6749e1d8d28ef7d210a677d589c6d1
Stefan <Stefanbotha1@gmail.com>
2024-09-02 21:57:44 -0400

    Task 1-4 completed for lab (review code and wrote code for add-ons function

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReducer from '/avslice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
   },
 });
